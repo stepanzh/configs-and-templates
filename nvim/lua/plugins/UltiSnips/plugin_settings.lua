020612f94f69ffdb4977a658c9ca1866f87021d9
stepanzh <stepanzh@gmail.com>
2025-09-27 16:10:31 +0300

    feat(nvim/snippets): cross-platform path to directory with snippets

--- nvim/lua/plugins/UltiSnips/plugin_settings.lua
+++ nvim/lua/plugins/UltiSnips/plugin_settings.lua
@@ -1,4 +1,5 @@
-vim.g.UltiSnipsSnippetDirectories = { vim.fn.expand('$HOME/.config/nvim/lua/plugins/UltiSnips') }
+-- One directory for all snippets, and it is directory where this file located
+vim.g.UltiSnipsSnippetDirectories = { debug.getinfo(1).source:match("@?(.*/)") }
 vim.g.UltiSnipsExpandTrigger = '<Tab>'
 vim.g.UltiSnipsJumpForwardTrigger = '<Tab>'
 vim.g.UltiSnipsJumpBackwardTrigger = '<S-Tab>'
